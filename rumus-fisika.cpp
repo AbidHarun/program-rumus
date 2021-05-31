59caa01b45d09c123bda8720ac0f60f61dc14c4d
AbidHarun <85109796+AbidHarun@users.noreply.github.com>
2021-05-31 19:55:16 +0800

    membuat file rumus-fisika.cpp
    membuat file rumus-fisika.cpp yang di dalamnya ada rumus gaya

---
+++ rumus-fisika.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int gaya (int m, int a)
+{
+    return (m*a);
+}
+int main ()
+{
+    int massa, percepatan;
+    cout<<"MENGHITUNG GAYA"<<endl;
+    cout<<endl;
+    cout<<" Massa Benda  : ";cin>>massa;
+    cout<<" Percepatan   : ";cin>>percepatan;
+    cout<<endl;
+    cout<<"Jadi Gayanya = "<<ep(massa,percepatan)<<" Newton"<<endl;
+}
